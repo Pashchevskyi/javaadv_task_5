2aa449aa5d7329b863ebde86a6c9a5a73eec0daf
Pavlo Pashchevskyi <googalltooth4@gmail.com>
2023-07-03 23:27:57 +0300

    EmployeeDocumentable and EmployeeControllable interface in EmployeeController

--- src/main/java/com/example/javaadv_task_5/web/EmployeeController.java
+++ src/main/java/com/example/javaadv_task_5/web/EmployeeController.java
@@ -7,10 +7,8 @@ import com.example.javaadv_task_5.dto.EmployeeOnlyDto;
 import com.example.javaadv_task_5.dto.EmployeeReadDto;
 import com.example.javaadv_task_5.service.EmployeeService;
 import com.example.javaadv_task_5.util.config.EmployeeMapper;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import com.example.javaadv_task_5.web.api.EmployeeControllable;
+import com.example.javaadv_task_5.web.api.EmployeeDocumentable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -34,8 +32,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
-@Tag(name = "Employee", description = "Employee API")
-public class EmployeeController {
+public class EmployeeController implements EmployeeControllable, EmployeeDocumentable {
 
     private final EmployeeService employeeService;
     private final EmployeeMapper mapper = EmployeeMapper.INSTANCE;
@@ -45,29 +42,27 @@ public class EmployeeController {
     }
 
     //Операция сохранения юзера в базу данных
+    @Override
     @PostMapping("/users")
     @ResponseStatus(HttpStatus.CREATED)
-    @Operation(summary = "This is endpoint to add a new employee.", description = "Create request to add a new employee.", tags = {"Employee"})
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "CREATED. The new employee is successfully created and added to database."),
-            @ApiResponse(responseCode = "400", description = "Invalid input"),
-            @ApiResponse(responseCode = "404", description = "NOT FOUND. Specified employee request not found."),
-            @ApiResponse(responseCode = "409", description = "Employee already exists")})
-    public EmployeeDto saveEmployee(@RequestBody @Valid EmployeeDto requestForSave) {
+    public EmployeeDto saveEmployeeWithMapping(@RequestBody @Valid EmployeeDto requestForSave) {
 
         Employee employee = mapper.employeeDtoToEmployee(requestForSave);
         EmployeeDto dto = mapper.employeeToEmployeeDto(employeeService.create(employee));
 
         return dto;
     }
+
+    @Override
     @PostMapping("/usersS")
     @ResponseStatus(HttpStatus.CREATED)
-    public void saveEmployee1(@RequestBody EmployeeDto employeeDto) {
+    public void saveEmployee(@RequestBody EmployeeDto employeeDto) {
         Employee employee = mapper.employeeDtoToEmployee(employeeDto);
         employeeService.create(employee);
     }
 
     //Получение списка юзеров
+    @Override
     @GetMapping("/users")
     @ResponseStatus(HttpStatus.OK)
     public List<EmployeeReadDto> getAllUsers() {
@@ -77,6 +72,7 @@ public class EmployeeController {
         return employeesReadDto;
     }
 
+    @Override
     @GetMapping("/users/p")
     @ResponseStatus(HttpStatus.OK)
     public Page<EmployeeReadDto> getPage(@RequestParam(defaultValue = "0") int page,
@@ -87,36 +83,35 @@ public class EmployeeController {
     }
 
     //Получения юзера по id
+    @Override
     @GetMapping("/users/{id}")
     @ResponseStatus(HttpStatus.OK)
-    @Operation(summary = "This is endpoint returned a employee by his id.", description = "Create request to read a employee by id", tags = {"Employee"})
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "OK. pam pam param."),
-            @ApiResponse(responseCode = "400", description = "Invalid input"),
-            @ApiResponse(responseCode = "404", description = "NOT FOUND. Specified employee request not found."),
-            @ApiResponse(responseCode = "409", description = "Employee already exists")})
     public EmployeeReadDto getEmployeeById(@PathVariable Integer id) {
         Employee employee = employeeService.getById(id);
         return mapper.employeeToEmployeeReadDto(employee);
     }
 
+    @Override
     @PatchMapping("/users/{id}/name")
     @ResponseStatus(HttpStatus.OK)
     public EmployeeDto refreshName(@PathVariable Integer id, @RequestBody EmployeeDto eDto) {
         return mapper.employeeToEmployeeDto(employeeService.updateNameById(id, eDto.getName()));
     }
 
+    @Override
     @PatchMapping("/users/{id}/email")
     @ResponseStatus(HttpStatus.OK)
     public EmployeeDto refreshEmail(@PathVariable Integer id, @RequestBody EmployeeDto eDto) {
         return mapper.employeeToEmployeeDto(employeeService.updateEmailById(id, eDto.getEmail()));
     }
 
+    @Override
     @PatchMapping("/users/{id}/country")
     public EmployeeDto refreshCountry(@PathVariable Integer id, @RequestBody EmployeeDto eDto) {
         return mapper.employeeToEmployeeDto(employeeService.updateCountryById(id, eDto.getCountry()));
     }
 
+    @Override
     @GetMapping("/users/email")
     @ResponseStatus(HttpStatus.OK)
     public List<EmployeeOnlyDto> getEmployeesByEmail(@RequestBody EmployeeEmailDto employeeEmailDto) {
@@ -126,6 +121,7 @@ public class EmployeeController {
         return list;
     }
 
+    @Override
     @GetMapping("/users/no-email")
     @ResponseStatus(HttpStatus.OK)
     public List<EmployeeReadDto> getEmployeeByEmailNull() {
@@ -135,6 +131,7 @@ public class EmployeeController {
         return list;
     }
 
+    @Override
     @PatchMapping("/users/fix-countries")
     @ResponseStatus(HttpStatus.OK)
     public List<EmployeeReadDto> fixCountriesNames() {
@@ -148,6 +145,7 @@ public class EmployeeController {
 
 
     //Удаление по id
+    @Override
     @PatchMapping("/users/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void removeEmployeeById(@PathVariable Integer id) {
@@ -155,12 +153,14 @@ public class EmployeeController {
     }
 
     //Удаление всех юзеров
+    @Override
     @DeleteMapping("/users")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void removeAllUsers() {
         employeeService.removeAll();
     }
 
+    @Override
     @GetMapping("/users/country")
     @ResponseStatus(HttpStatus.OK)
     public Page<EmployeeReadDto> findByCountry(@RequestParam(required = false) String country,
@@ -174,12 +174,14 @@ public class EmployeeController {
             sortOrder.toString()).map(mapper::employeeToEmployeeReadDto);
     }
 
+    @Override
     @GetMapping("/users/c")
     @ResponseStatus(HttpStatus.OK)
     public List<String> getAllUsersC() {
         return employeeService.getAllEmployeeCountry();
     }
 
+    @Override
     @GetMapping("/users/s")
     @ResponseStatus(HttpStatus.OK)
     public List<String> getAllUsersSort() {
@@ -192,6 +194,7 @@ public class EmployeeController {
         return employeeService.findEmails();
     }
 
+    @Override
     @GetMapping("/users/countryBy")
     @ResponseStatus(HttpStatus.OK)
     public List<EmployeeReadDto> getByCountry(@RequestParam(required = true) String country) {
